1a096adb04ca7eafc577a1f7322b733ea1f14cce
Lijia, Tang <barrytan@amazon.com>
2024-10-05 15:16:56 +0800

    bug

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # fping-server
 fping central server
+
+# setup
+
+```bash
+pip install fastapi
+pip install "uvicorn[standard]"
+```
+
+# run
+
+```bash
+uvicorn main:app --reload
+```
+
+
--- main.py
+++ main.py
@@ -1,8 +1,44 @@
 from fastapi import FastAPI
+from fastapi import Request
+from pydantic import BaseModel
+#import uvicorn
+
+class JobResult(BaseModel):
+    jobid: str
+    stdout: str
+    stderr: str
 
 app = FastAPI()
 
+@app.get("/job")
+async def job():
+    return {
+        "job": [
+            {
+                "jobid": "123-1",
+                "command": "fping --help",
+            },
+            {
+                "jobid": "123-2",
+                "command": "fping -g 1.1.1.0 1.1.1.3 -r 2 -a -q",
+            },
+            {
+                "jobid": "123-3",
+                "command": "fping -g 110.242.68.1 110.242.68.10 -r 2 -a -q",
+            },
+        ],
+        "next": "sfkR1xjer",
+        "interval": 10,
+        "status": 200
+    }
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.post("/job")
+async def postjob(request: Request): #jobRet: JobResult
+    print(request)
+    #jobResult = jobRet.dict()
+    jobResult = await request.json()
+    print(jobResult)
+    return job()
+
+#if __name__ == "__main__":
+#    uvicorn.run(app, host="0.0.0.0", port=8080)
